ee82e83169854099701c7f21bb9a1eeb20fd6c22
Adeline Almanzar <almanzar.adeline@gmail.com>
2022-03-04 11:32:59 -0800

    commit tree 8865de693796aeabc21e22f37babe25bf71d66bc

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,28 @@
 class ApplicationController < Sinatra::Base
+  set :default_content_type, 'application/json'
 
-  # add routes
+  get '/' do
+    "Hello World"
+  end
+
+  get '/bakeries' do
+    bakeries = Bakery.all
+    bakeries.to_json
+  end
+
+  get '/bakeries/:id' do
+    bakery = Bakery.all.find_by(params[:id])
+    bakery.to_json(include: :baked_goods)
+  end
+
+  get '/baked_goods/by_price' do
+    baked_goods = BakedGood.all.order(price: :desc)
+    baked_goods.to_json
+  end
+
+  get '/baked_goods/most_expensive' do
+    baked_goods = BakedGood.all.order(price: :desc).first
+    baked_goods.to_json
+  end
 
 end
